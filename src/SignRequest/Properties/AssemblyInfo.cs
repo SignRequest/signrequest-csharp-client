70c34d6a735c5550c021d0325620fb59696f4555
Chris Jackson <chris@signrequest.com>
2018-10-31 15:40:44 +0100

    Update AssemblyInfo

--- src/SignRequest/Properties/AssemblyInfo.cs
+++ src/SignRequest/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Swagger Library")]
-[assembly: AssemblyDescription("A library generated from a Swagger doc")]
+[assembly: AssemblyTitle("SignRequest API Client")]
+[assembly: AssemblyDescription("Official API client for SignRequest.com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Swagger")]
-[assembly: AssemblyProduct("SwaggerLibrary")]
+[assembly: AssemblyCompany("SignRequest")]
+[assembly: AssemblyProduct("SignRequest.com")]
 [assembly: AssemblyCopyright("No Copyright")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
